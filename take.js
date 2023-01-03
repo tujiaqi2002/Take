55b8b288c014500db0e72e80487b3eec18e4c8f8
Tusu <321756623tu@gmail.com>
2023-01-03 03:27:55 -0500

    complete the HP

--- take.js
+++ take.js
@@ -6,9 +6,10 @@ var context;
 var boardHeight = 1000;
 var boardWidth = 1000;
 var playerRadius = 15;
+var playerCoord;
 
-var HP = 100;
-var enermyAttack = 5;
+var HP = 30;
+var enermyAttack = 0;
 
 console.log(board);
 
@@ -25,7 +26,7 @@ function gameStart() {
   context.fillRect(0, 0, board.width, board.height);
 
   //draw Player
-  var playerCoord = new Coordinate(boardWidth / 2 - 1, boardHeight / 2 - 1);
+  playerCoord = new Coordinate(boardWidth / 2 - 1, boardHeight / 2 - 1);
   context.fillStyle = "#FFFFFF";
   context.strokeStyle = "#FFFFFF";
   context.beginPath();
@@ -65,19 +66,78 @@ function gameStart() {
 
   context.closePath();
   context.stroke();
-  setInterval(update, 1000 / 60);
+  setInterval(update, 1000);
 }
 
-function update() {
+function refreshGame() {
+  //draw game board
+  context.fillStyle = "#000000";
+  context.fillRect(0, 0, board.width, board.height);
+
+  //draw Player
+  playerCoord = new Coordinate(boardWidth / 2 - 1, boardHeight / 2 - 1);
   context.fillStyle = "#FFFFFF";
-  context.fillRect(
+  context.strokeStyle = "#FFFFFF";
+  context.beginPath();
+  context.arc(
+    playerCoord.x,
+    playerCoord.y,
+    playerRadius,
+    0,
+    2 * Math.PI,
+    false
+  );
+  context.stroke();
+  context.fill();
+
+  //draw HP bar
+  context.strokeStyle = "#FFFFFF";
+  context.beginPath();
+  context.moveTo(
     playerCoord.x - playerRadius,
-    playerCoord.y - playerRadius - 20,
+    playerCoord.y - playerRadius - 10
+  );
+
+  context.lineTo(
     playerCoord.x + playerRadius,
     playerCoord.y - playerRadius - 10
   );
 
-  window.addEventListener("keypress", playerMove);
+  context.lineTo(
+    playerCoord.x + playerRadius,
+    playerCoord.y - playerRadius - 20
+  );
+
+  context.lineTo(
+    playerCoord.x - playerRadius,
+    playerCoord.y - playerRadius - 20
+  );
+
+  context.closePath();
+  context.stroke();
 }
 
-function playerMove() {}
+function update() {
+  refreshGame();
+  //   window.addEventListener("keydup", playerMove);
+  context = board.getContext("2d");
+  context.fillStyle = "#FFFFFF";
+  console.log(playerCoord.x - playerRadius);
+
+  context.fillRect(
+    playerCoord.x - playerRadius,
+    playerCoord.y - playerRadius - 20,
+    HP,
+    10
+  );
+  console.log(HP);
+  HP = HP - enermyAttack;
+  if (HP <= 0) {
+    alert("lose ");
+  }
+  window.addEventListener("keyup", playerMove);
+}
+
+function playerMove(e) {
+  console.log(e.key);
+}
